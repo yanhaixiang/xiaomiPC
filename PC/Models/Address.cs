d05e06f8d70c9f008e04e3efee97fc6465c79e67
yanhaixiang <1729628996@qq.com>
2019-04-15 10:35:04 +0800

    tj

--- PC/Models/Address.cs
+++ PC/Models/Address.cs
@@ -12,5 +12,6 @@ namespace PC.Models
         public string HarvestName { get; set; }
         public string AddressDetail { get; set; }
         public string Phone { get; set; }
+        public int UserId { get; set; }
     }
 }
